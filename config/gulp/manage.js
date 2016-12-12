24e7d7d9d77ac82e38ab18f40b66a5fa92a48d29
JPeer264 <jan.oster94@gmail.com>
2016-12-12 08:46:20 +0100

    added paths into gulpfile, trying to read them as in grunt

--- config/gulp/manage.js
+++ config/gulp/manage.js
@@ -1,13 +1,22 @@
 module.exports = function (options) {
-    const gulp        = options.gulp;
-    const plugins     = options.plugins;
-    const directories = options.directories;
+    const gulp         = options.gulp;
+    const paths        = options.paths;
+    const plugins      = options.plugins;
+    const autoprefixer = require('autoprefixer');
 
     return {
         sass: function () {
-            return gulp.src(directories.src + '/assets/scss/main.scss')
+            const postcssProcessors = [
+                autoprefixer({ browsers: ['last 2 versions'] })
+            ];
+            console.log(paths)
+
+            var t = gulp.util.template('src/scss/main.scss', paths);
+            console.log(t)
+            return gulp.src('src/**/*.scss')
                 .pipe(plugins.sass())
-                .pipe(gulp.dest(directories.dev));
+                .pipe(plugins.postcss(postcssProcessors))
+                .pipe(gulp.dest('dev'));
         }
     };
 };
