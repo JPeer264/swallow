d4239771efbc5a57deaee0ccb24567bd670d73cb
JPeer264 <jan.oster94@gmail.com>
2016-12-28 20:17:25 +0100

    updated doc

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,11 @@
 'use strict';
 
+// @todo find alternative for finding recursive template strings
+// remove grunt as template finder
+
+/*************
+ ** MODULES **
+ *************/
 const _       = require('lodash');
 const gulp    = require('gulp');
 const grunt   = require('grunt');
@@ -9,19 +15,32 @@ const plugins = require('gulp-load-plugins')();
 const deleteEmpty = require('delete-empty');
 const browserSync = require('browser-sync').create();
 
-gulp.util   = require('gulp-util');
-gulp.util._ = _;
-gulp.data   = grunt.config;
-gulp.data.init(paths);
-
+/**
+ * get a task in ./config/gulp/
+ *
+ * @param  {string} mainTask the filename for the task
+ * @param  {string} subTask  the returned task from the filename
+ *
+ * @return {Stream}
+ */
 const getTask = (mainTask, subTask) => {
     const task = require('./config/gulp/' + mainTask)({gulp, plugins, paths});
 
     return task[subTask];
 };
 
+gulp.util   = require('gulp-util');
+gulp.util._ = _;
+gulp.data   = grunt.config;
+gulp.data.init(paths);
+
+/***********
+ ** TASKS **
+ ***********/
 gulp.task('default', ['build:prod']);
 
+// 0. Helper
+// ---------
 gulp.task('clean', () => {
     return gulp.src(_.flatten([
             gulp.data.get('paths.dev.base'),
@@ -32,14 +51,16 @@ gulp.task('clean', () => {
         .pipe(plugins.clean());
 });
 
-// managing
+// 1. Managing
+// -----------
 gulp.task('manage', ['manage:sass', 'manage:js:vendor', 'manage:js']);
 gulp.task('manage:js', getTask('manage', 'js:own'));
 gulp.task('manage:js:vendor', getTask('manage', 'js:vendor'));
 gulp.task('manage:sass', ['manage:sass:browser'], getTask('manage', 'sass'));
 gulp.task('manage:sass:browser', getTask('manage', 'sass:browser'));
 
-// linting
+// 2. Linting
+// ----------
 gulp.task('lint', ['lint:js', 'lint:scss', 'lint:html']);
 gulp.task('lint:fail', ['lint:js:fail', 'lint:scss:fail', 'lint:html:fail']);
 gulp.task('lint:js', getTask('lint', 'js'));
@@ -49,16 +70,19 @@ gulp.task('lint:scss:fail', getTask('lint', 'scss:fail'));
 gulp.task('lint:html', getTask('lint', 'html'));
 gulp.task('lint:html:fail', getTask('lint', 'html:fail'));
 
-// minifying
+// 3. Minifying
+// ------------
 gulp.task('minify', ['minify:css', 'minify:js', 'minify:html']);
 gulp.task('minify:js', ['manage:js', 'minify:css'], getTask('minify', 'js'));
 gulp.task('minify:css', ['manage:sass'], getTask('minify', 'css'));
 gulp.task('minify:html', ['minify:css'], getTask('minify', 'html'));
 
-// test
+// 4. Testing
+// ----------
 gulp.task('test', ['manage:js:vendor'], getTask('test', 'all'));
 
-// reports
+// 5. Reports
+// ----------
 gulp.task('reports', ['reports:test', 'reports:lint']);
 gulp.task('reports:test', ['test']);
 gulp.task('reports:lint', () => {
@@ -70,7 +94,8 @@ gulp.task('reports:lint', () => {
     return stream;
 });
 
-// build
+// 6. Build
+// --------
 gulp.task('build', ['build:prod']);
 
 gulp.task('build:dev', ['manage'], () => {
@@ -111,7 +136,8 @@ gulp.task('build:prod', ['test', 'lint:fail'], () => {
     });
 });
 
-// serve
+// 7. Serve
+// --------
 gulp.task('serve', ['serve:dev']);
 
 gulp.task('serve:dev',['build:dev'], () => {
