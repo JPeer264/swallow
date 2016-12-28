ddca700c5107f180c29005db5b09c7f0de9f328a
JPeer264 <jan.oster94@gmail.com>
2016-12-28 15:45:10 +0100

    added linting reports for scss and js

--- config/gulp/lint.js
+++ config/gulp/lint.js
@@ -2,10 +2,11 @@ module.exports = options => {
     const gulp    = options.gulp;
     const paths   = options.paths;
     const plugins = options.plugins;
+    const fs = require('fs-extra');
 
     // lints scss files but does not fail
     const scss = () => {
-        return gulp.src(gulp.data.get('paths.src.allFiles.scss'))
+        return stream = gulp.src(gulp.data.get('paths.src.allFiles.scss'))
             .pipe(plugins.sassLint({
                 configFile: gulp.data.get('paths.config.scsslint')
             }))
@@ -15,6 +16,29 @@ module.exports = options => {
     // lints scss files and fails
     const scssFail = () => {
         return scss().pipe(plugins.sassLint.failOnError());
+    };
+
+    // lints scss files and create a report
+    const scssReport = () => {
+        const filePath = gulp.data.get('paths.reports.scss');
+
+        fs.ensureFileSync(filePath);
+
+        const fileStream = fs.createWriteStream(filePath);
+        const stream = gulp.src(gulp.data.get('paths.src.allFiles.scss'))
+            .pipe(plugins.sassLint({
+                configFile: gulp.data.get('paths.config.scsslint'),
+                options: {
+                    formatter: 'checkstyle'
+                }
+            }))
+            .pipe(plugins.sassLint.format(fileStream));
+
+        stream.on('finish', function() {
+            fileStream.end();
+        });
+
+        return stream;
     };
 
     // lints js files but does not fail
@@ -29,6 +53,26 @@ module.exports = options => {
     // lints js files and fails
     const jsFail = () => {
         return js().pipe(plugins.eslint.failOnError());
+    };
+
+    // lints js files and create a report
+    const jsReport = () => {
+        const filePath = gulp.data.get('paths.reports.js');
+
+        fs.ensureFileSync(filePath);
+
+        const fileStream = fs.createWriteStream(filePath);
+        const stream = gulp.src(gulp.util._.flatten(gulp.data.get('paths.src.files.js')))
+            .pipe(plugins.eslint({
+                configFile: gulp.data.get('paths.config.eslint')
+            }))
+            .pipe(plugins.eslint.format('checkstyle', fileStream));
+
+        stream.on('finish', function() {
+            fileStream.end();
+        });
+
+        return stream;
     };
 
     // lints html files but does not fail
@@ -52,8 +96,10 @@ module.exports = options => {
     return {
         scss,
         'scss:fail': scssFail,
+        'scss:report': scssReport,
         js,
         'js:fail': jsFail,
+        'js:report': jsReport,
         html,
         'html:fail': htmlFail
     };
